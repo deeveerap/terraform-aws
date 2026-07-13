d9bb7f4f5e25501cb9b030aa886af3f81ff9b7ba
deeveerap <39435095+deeveerap@users.noreply.github.com>
2026-07-13 16:50:47 +0530

    Update output.tf

--- create_vpc_subnet_ec2/output.tf
+++ create_vpc_subnet_ec2/output.tf
@@ -1,9 +1,9 @@
-output "kke_vpc_name" {
+output "KKE_vpc_name" {
   value       = aws_vpc.main.tags["Name"]
   description = "The unique identifier of the deployed VPC"
 }
 
-output "kke_subnet_name" {
+output "KKE_subnet_name" {
   value       = aws_subnet.mains.tags["Name"]
   description = "The unique identifier of the deployed Subnet"
 }
